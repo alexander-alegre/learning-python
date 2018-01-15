7254ae6cd811ee4b61d563b738b2019c9aa22a04
Alex Alegre <alexander.alegre@outlook.com>
2018-01-14 22:05:56 -0700

    initial commit

---
+++ refresher/variables_methods.py
@@ -0,0 +1,23 @@
+a = 5
+b = 10
+
+my_variable = 2345
+any_variable_name = 'hello'
+
+# print(a)
+# print(my_variable)
+
+###
+
+def my_print_method(greeting):
+    print(greeting)
+
+
+my_print_method('hello world')
+
+def my_multi_method(a, b):
+    return a * b
+
+result = my_multi_method(2, 9)
+
+my_print_method(result)
