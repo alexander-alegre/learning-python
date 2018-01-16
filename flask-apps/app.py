559fc4c20c669bf0e5a35a8b2695db07bcf6d28b
Alex Alegre <alexander.alegre@outlook.com>
2018-01-15 23:57:08 -0700

    create GET request in flask

--- flask-apps/app.py
+++ flask-apps/app.py
@@ -1,9 +1,42 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello world'
+stores = [
+    {
+        'name': 'Apple Store',
+        'items': [
+            {
+                'name': 'MacBook Pro',
+                'price': 1999.00
+            }
+        ]
+    }
+]
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+
+@app.route('/store')
+def get_stores():
+    return jsonify({ 'stores': stores })
+
+
+@app.route('/store/<string:name>item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
+
 
 app.run(port=5000)
