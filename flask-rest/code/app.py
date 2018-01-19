9d8ffd0bcd83fe88325d69f77edeb2ebaf15e636
Alex Alegre <alexander.alegre@outlook.com>
2018-01-19 07:54:11 -0700

    add better error handling and response codes

--- flask-rest/code/app.py
+++ flask-rest/code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -11,14 +11,17 @@ errorMessage = { 'code': 404, 'message': 'Item not found' }
 class Item(Resource):
     # return an item by name
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item, 200
-        return errorMessage, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return { 'item': item }, 200 if item else 404
 
     # create an item with a name
     def post(self, name):
-        item = { 'name': name, 'price': 12.00 }
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return { 'message': 'An item with name "{}" already exists.'.format(name) }, 400
+
+
+        data = request.get_json()
+        item = { 'name': name, 'price': data['price'] }
         items.append(item)
         return successMessage, 201
 
@@ -33,4 +36,4 @@ class Items(Resource):
 api.add_resource(Items, '/items')
 
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
