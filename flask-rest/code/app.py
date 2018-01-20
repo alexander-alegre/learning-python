d9ce5a121307931814bb5cec9cfa6f08f86a8394
Alex Alegre <alexander.alegre@outlook.com>
2018-01-20 08:41:53 -0700

    complete routes and authenticate all

--- flask-rest/code/app.py
+++ flask-rest/code/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -15,34 +15,56 @@ successMessage = { 'code': 200, 'message': 'Success' }
 errorMessage = { 'code': 404, 'message': 'Item not found' }
 
 class Item(Resource):
-    @jwt_required()
-    # return an item by name
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank"
+    )
+
+    @jwt_required() # this will make it so the request requires authentication
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return { 'item': item }, 200 if item else 404
 
-    # create an item with a name
+
+    @jwt_required()
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
             return { 'message': 'An item with name "{}" already exists.'.format(name) }, 400
 
-
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = { 'name': name, 'price': data['price'] }
         items.append(item)
         return successMessage, 201
 
+
+    @jwt_required()
     def delete(self, name):
         global items
         items = list(filter(lambda x: x['name'] != name, items))
         return { 'message': 'Item deleted: "{}"'.format(name) }
 
 
+    @jwt_required()
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = { 'name': name, 'price': data['price'] }
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+
 api.add_resource(Item, '/item/<string:name>')
 
 
 class Items(Resource):
-    # retrieve all items 
+    # retrieve all items
+    @jwt_required()
     def get(self):
         return items, 200
 
