d130fa839b43f610a26bc3891deee9456db9c051
Alex Alegre <alexander.alegre@outlook.com>
2018-01-20 08:14:04 -0700

    implement authentication

--- flask-rest/code/app.py
+++ flask-rest/code/app.py
@@ -1,14 +1,21 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'secret'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 successMessage = { 'code': 200, 'message': 'Success' }
 errorMessage = { 'code': 404, 'message': 'Item not found' }
 
 class Item(Resource):
+    @jwt_required()
     # return an item by name
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
